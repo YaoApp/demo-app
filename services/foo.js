774437f183afacfdd839c799e7f3b59fb7782414
Max <shadow.iqka@qq.com>
2022-10-17 22:16:51 +0800

    [add]  test service

---
+++ services/foo.js
@@ -0,0 +1,12 @@
+/**
+ * Cloud Function
+ *
+ * POST /api/__yao/app/service/foo
+ * {"method":"Bar", "args":["hello", "world"]}
+ *
+ * @param  {...any} args
+ * @returns
+ */
+function Bar(...args) {
+  return args;
+}
